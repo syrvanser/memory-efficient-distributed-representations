57f98e9b4cdd49825013d98e90e3b3ec0bbb8a4d
Ivan Syrovoiskii <syrvanser@gmail.com>
2021-07-02 13:56:01 +0100

    fixed script files

--- run_dpq.sh
+++ run_dpq.sh
@@ -10,6 +10,7 @@ num_negatives=4
 l2_norm=0.00002
 mlp_1=32
 mlp_2=16
+mlp_3=8
 dataset=100k
 exp_dir=dpq
 model=dpq
@@ -18,4 +19,4 @@ k=32
 d=8
 shared_centroids=true
 
-python main.py --batch_size=$batch_size --epochs=$epochs --learning_rate=$learning_rate --seed=$seed --embedding_dimensions=$embedding_dimensions --k=$k --d=$d --shared_centroids=$shared_centroids --num_negatives=$num_negatives --mlp_1=$mlp_1 --mlp_2=$mlp_2 --dataset=$dataset --exp_dir=$exp_dir --model=$model
+python main.py --batch_size=$batch_size --epochs=$epochs --learning_rate=$learning_rate --seed=$seed --embedding_dimensions=$embedding_dimensions --l2_norm=$l2_norm --num_negatives=$num_negatives --mlp_1=$mlp_1 --mlp_2=$mlp_2 --mlp_3=$mlp_3 --dataset=$dataset --exp_dir=$exp_dir --model=$model
